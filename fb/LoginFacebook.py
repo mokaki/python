da7158b87c7232e40ce9e5c46b319c1eec7eaea0
DustGroup <56112369+mokaki@users.noreply.github.com>
2020-07-21 22:23:35 +0800

    Update LoginFacebook.py

--- fb/LoginFacebook.py
+++ fb/LoginFacebook.py
@@ -1,10 +1,28 @@
-
+'''
+LoginFacebook
+#https://www.guru99.com/facebook-login-using-python.html
+#https://stackoverflow.max-everyday.com/2019/12/selenium-chrome-options/
+'''
+#!/usr/bin/python3
+#!python*
+# -*- coding: utf-8 -*-
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
+
+###############################################
+options = webdriver.ChromeOptions() 
+#禁用瀏覽器彈窗
+prefs = {
+     'profile.default_content_setting_values' :  {  
+         'notifications' : 2  
+      }  
+ }  
+options.add_experimental_option('prefs',prefs)
+
 # Step 1) Open Firefox 
-browser = webdriver.Chrome('../.exe/chromedriver')
+browser = webdriver.Chrome('../.exe/chromedriver' ,chrome_options=options)
 browser.set_window_size(480, 320)
 
 # Step 2) Navigate to Facebook
@@ -14,18 +32,17 @@ browser.get("http://www.facebook.com")
 username = browser.find_element_by_id("email")
 password = browser.find_element_by_id("pass")
 submit   = browser.find_element_by_id("loginbutton")
-username.send_keys("you@mail")
-password.send_keys("youpwss")
+username.send_keys("l0000000@gmail.com")#####################################
+password.send_keys("h600000password0000005")#####################################
 
 # Step 4) Click Login
 submit.click()
 
 
-
-
-
 '''
-https://www.guru99.com/facebook-login-using-python.html
+LoginFacebookEND
 '''
 
 
+
+print ('LoginFacebookEND')
