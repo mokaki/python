1d711d2fe548af987488aeac5260e25f3ae8d1be
DustGroup <56112369+mokaki@users.noreply.github.com>
2020-09-13 01:06:45 +0800

    Create jobsgovhk.py

---
+++ GetDate/jobsgovhk.py
@@ -0,0 +1,136 @@
+
+#聯資
+#!/usr/bin/python3
+#!python*
+# -*- coding: utf-8 -*-
+
+
+#import
+import requests
+from bs4 import BeautifulSoup
+import os 
+import json
+import time
+import datetime
+import random
+from tkinter import *
+import tkinter as tk
+from functools import partial
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+DataURL = "../date/"
+DataURL1 = "聯資.txt"
+
+today = datetime.date.today()
+dayOfe = str(today)[0:4] #今年號
+dayOfm = str(int(str(today)[5:7])) #今月號/0
+dayOfd = str(today)[8:11] #今日號
+url01 = 'https://www1.jobs.gov.hk/1/0/WebForm/jobseeker/jobsearch/quickview.aspx?SearchFor=vac_by_date&ID=' #勞網
+
+#網code
+WebDriverWaitMSG = "\n******************!!!找不到這個網頁原素!!!******************\n"
+WebDriverWaitMSGEND = "\n************************************************************"
+co0 = str('//*[@id="ctl00_ContentPlaceHolder1_uxTotalPage"]/span[1]')#空缺數
+co1 = str('//*[@id="ctl00_ContentPlaceHolder1_uxJobCard_uxAppNote"]')
+
+c91 = str('//*[@id="uxResult"]/tbody/tr[')
+c92 = str(']')
+c93 = str('&SortBy=&from=&start=')
+
+
+
+
+
+#_seeToDay#_seeToDay#_seeToDay#_seeToDay#_seeToDay#_seeToDay#_seeToDay#_seeToDay
+def _seeToDay():
+	#瀏覽器
+	options = webdriver.ChromeOptions() 
+	prefs = {'profile.default_content_setting_values':{'notifications' : 2}}  #禁用瀏覽器彈窗
+	options.add_experimental_option('prefs',prefs)
+	options.add_argument('--headless')#冇頭
+	options.add_argument('--no-sandbox')#冇頭
+	browser = webdriver.Chrome('../.exe/chromedriver' ,chrome_options=options)
+	#browser.set_window_size(480, 600)
+
+
+
+
+	#input日期,沒=今日
+	name = input('現在是'+time.strftime('%Y年%m月')+'請問您需要獲得那一天資料*冇0')
+	if name == '':
+		#網址=url01 + dayOfd + '/' + dayOfm + '/' + dayOfe
+		allurl = str(url01 + dayOfd + '/' + dayOfm + '/' + dayOfe)
+	else:
+		allurl = str(url01 + name + '/' + dayOfm + '/' + dayOfe)
+
+	print(allurl)
+	os.system("pause")
+
+
+
+	browser.get(allurl)#打開app,網址+日期自動
+	ss0 = (browser.find_element_by_xpath(co0)).text#空缺數#用空缺數驗吉頁
+	if int(ss0) == 0:
+		print('\n*****************',time.strftime('%Y%m'),'-',int(name),'暫時沒有資料*****************\n')
+		os.system("pause")
+	else:
+
+		DataURL00 = str(DataURL+time.strftime('%Y%m%d-%H')+DataURL1)#文件名
+		fp = open(DataURL00  , "a", encoding="utf-8" )#打開文件
+		fp.writelines(str(time.strftime('%Y%m%d--%H%M%S'))+'\n')
+		fp.close()#关闭文件
+
+		#每一頁
+		SortBy = 0#一頁20工,下頁+20
+		while (int(ss0) != 0): #click20次後轉頁=allurl+20=allurlN20
+			count = 1
+
+				#click20次搜尋結果排序 1~20
+			while (count <= 20): 
+				element = WebDriverWait(browser, 10, 0.5).until(#沒co1會報錯
+						EC.presence_of_element_located((By.XPATH,co1)),WebDriverWaitMSG + co1 + WebDriverWaitMSGEND
+					)
+				d01 = browser.find_element_by_xpath(co1).text#申請須知聯資
+				print(d01)#聯資#聯資#聯資#聯資#聯資#聯資#聯資#聯資
+				fp = open(DataURL00  , "a", encoding="utf-8" )#打開文件
+				fp.writelines('\n'+str(d01))
+				fp.close()#关闭文件
+				count += 1
+				count00 = str(count)
+				allc9 = str(c91 + count00 + c92)#搜尋結果下一個
+				ss1 = browser.find_element_by_xpath(allc9)
+				ss1.click()
+				if (count == 20): #讓迴圈結束的條件
+					break
+			#click20次搜尋結果排序 1~20END
+
+			SortBy += 20#下頁+20
+			SortBy2 = SortBy + 20
+			print('\n*****************' ,SortBy , '至' , SortBy2 , '*****************\n')
+			allurlN20 = str(allurl + c93 + str(SortBy))#搜尋結果下一頁
+			browser.get(allurlN20)
+			ss0 = (browser.find_element_by_xpath(co0)).text#空缺數
+			if (int(ss0) == 00): #讓迴圈結束的條件
+				break
+		#每一頁END
+
+		fp = open(DataURL00  , "a", encoding="utf-8" )#打開文件
+		fp.writelines('\n'+str(time.strftime('%Y%m%d--%H%M%S'))+'END')
+		fp.close()#关闭文件
+		print('\n',today,'資料已保存\n')
+
+		os.system("pause")
+
+
+
+
+_seeToDay()#打開app,網址+日期自動
+
+
+
