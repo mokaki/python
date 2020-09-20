c3f5150712dc96d987ec581eb660e6a5e0d2cc38
DustGroup <56112369+mokaki@users.noreply.github.com>
2020-09-21 02:50:07 +0800

    Add files via upload

---
+++ othen/去除txt文件中重复的行数.py
@@ -0,0 +1,15 @@
+﻿
+#coding:utf-8
+import shutil
+readDir = "88dbDate-生意頂讓202009.txt"
+writeDir = "a88dbDate-生意頂讓202009.txt"
+lines_seen = set()
+outfile=open(writeDir,"w+",encoding='utf-8-sig')
+f = open(readDir,"r",encoding='utf-8-sig').readlines()
+for line in f:
+    if line not in lines_seen:
+        outfile.write(line)
+        lines_seen.add(line)
+outfile.close()
+print('success')
+ 
