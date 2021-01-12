4e05b454c5bd13fedebe91cb1ecc67133c8a8ccb
DustGroup <56112369+mokaki@users.noreply.github.com>
2021-01-12 18:16:09 +0800

    Add files via upload

---
+++ othen/DWL_RAR.py
@@ -0,0 +1,29 @@
+﻿
+
+url = 'http://www.mobanwang.com/mb/showsoftdown.asp?urlid=1&softid=18061'
+ 
+# downloading with requests
+ 
+# import the requests library
+import requests
+ 
+
+
+
+
+# download the file contents in binary format
+r = requests.get(url)
+ 
+# open method to open a file on your system and write the contents
+with open("18061w.rar", "wb") as code:
+	
+	code.write(r.content)
+
+ 
+# downloading with urllib
+ 
+# import the urllib library
+import urllib
+ 
+# Copy a network object to a local file
+urllib.urlretrieve(url, "18061w.rar")
