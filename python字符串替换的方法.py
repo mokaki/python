0ec93e64bdcdfb928d58d9bf7f65bca6af5d89a7
DustGroup <56112369+mokaki@users.noreply.github.com>
2021-01-21 21:59:03 +0800

    Add files via upload

---
+++ python字符串替换的方法.py
@@ -0,0 +1,22 @@
+﻿
+
+#https://www.cnblogs.com/zdz8207/p/python-updateFile-re-sub.html
+
+import os
+def updateFile(file,old_str,new_str):
+    """
+    将替换的字符串写到一个新的文件中，然后将原文件删除，新文件改为原来文件的名字
+    :param file: 文件路径
+    :param old_str: 需要替换的字符串
+    :param new_str: 替换的字符串
+    :return: None
+    """
+    with open(file, "r", encoding="utf-8") as f1,open("%s.bak" % file, "w", encoding="utf-8") as f2:
+        for line in f1:
+            if old_str in line:
+                line = line.replace(old_str, new_str)
+            f2.write(line)
+    os.remove(file)
+    os.rename("%s.bak" % file, file)
+
+updateFile(r"index.html", "44444444", "2222222")#将"D:\zdz\"路径的myfile.txt文件把所有的zdz改为daziran
