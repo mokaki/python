dd07c84f768091e79e50da7659ce380486779278
DustGroup <56112369+mokaki@users.noreply.github.com>
2021-06-14 08:55:35 +0800

    Add files via upload

---
+++ othen/UnZipRAR.py
@@ -0,0 +1,28 @@
+﻿
+# https://tw.clearwatergardenclub.org/985574-how-to-read-a-text-YZVZWS
+
+
+import rarfile
+import os
+
+DataURL = "ind1ex.html"
+fp = open( DataURL , "a", encoding="utf-8" )	
+
+with rarfile.RarFile("18333.rar") as rf:
+	with rf.open("html/index.html") as f:
+		for ln in f:
+			
+
+			ewqqew1 = ln.strip()
+			ewqqew1 = ewqqew1.decode("utf-8")
+			ewqqew1 = ewqqew1.replace('><', '>\n<') 
+			print (ewqqew1)
+			fp.writelines(ewqqew1)
+
+fp.close()
+
+
+print ('\n己完成記錄 今日直播\n請按任意鍵退出')
+os.system("pause")
+
+
