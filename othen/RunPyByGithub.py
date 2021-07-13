a876a5aa985ac24bf10a8b4f6c4cd326c4616996
DustGroup <56112369+mokaki@users.noreply.github.com>
2021-07-14 00:10:08 +0800

    Add files via upload

---
+++ othen/RunPyByGithub.py
@@ -0,0 +1,27 @@
+﻿
+'''
+py3
+
+RunPyByGithub
+ATW202107140008
+mokaki
+
+pip install 
+
+https://stackoverflow.com/questions/54963794/read-python-code-from-github-and-execute-locally
+
+
+
+'''
+# -*- coding: UTF-8 -*-
+
+
+
+import urllib.request
+
+code = 'https://raw.githubusercontent.com/mokaki/AutoWeb/master/%E8%87%AA%E5%8B%95%E6%90%B5%E5%AE%A2.py'
+
+response = urllib.request.urlopen(code)
+data = response.read()
+
+exec(data)
